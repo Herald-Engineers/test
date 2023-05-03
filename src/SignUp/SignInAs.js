af29b4032aea470dc818807060a993ec73b79d3a
Sabin Lohani <np03cs4s220104@heraldcollege.edu.np>
2023-05-04 00:28:17 +0545

    Minor signup as page css fix

--- src/SignUp/SignInAs.js
+++ src/SignUp/SignInAs.js
@@ -9,43 +9,32 @@ import { Link } from 'react-router-dom';
 import MyImage from '../Image/logo123.png';
 
 function Signin() {
-    const myStyle = {
-        color: 'white',
-    };
-    // const location = useLocation();
-    // const { inputValue } = location.state || {};
+  const myStyle = {
+    color: 'white',
+  };
+  // const location = useLocation();
+  // const { inputValue } = location.state || {};
   return (
     <div>
       <div className='container-fluid'>
-        <img src={LoginSec} alt='Protect Water'  className='MyLogin' />
+        <img src={LoginSec} alt='Protect Water' className='MyLogin' />
       </div>
+
       <div className='d-flex justify-content-center text-center SignIn' >
-      <img src={MyImage} alt='Wave Billing System Logo'  className='myLogo text-center' />
+        <img src={MyImage} alt='Wave Billing System Logo' className='myLogo text-center' />
         <h1 >Sign Up</h1>
-        <p style={{color:'#BBB6B6'}}>Get Started with WaveBilling</p>
-        <div className="image-container">
-
-          <div className='box-container'>
-            <Link to='/user'><button className='btn btn-primary'>As a user</button></Link>
-            
-          </div>
-
-          <div className='box-container'>
-          <Link to='/register'><button className='myCompanyButton '>As a company</button></Link>
-            <p style={myStyle}>Company</p>
-          </div>
-
-          
-          <br></br>
-        </div>
+        <p style={{ color: '#BBB6B6' }}>Get Started with WaveBilling</p>
+        <Link to='/user'><button className='btn btn-primary'>As a user</button></Link>
+        <Link to='/register'><button className='myCompanyButton '>As a company</button></Link>
+        {/* <p style={myStyle}>Company</p> */}
         <div>
-          <p style={{color:'#B1ABAB', fontSize:'11px'}}>Already have an account? <Link to='/login'><span>Login Now</span></Link></p>
-        </div> 
+          <p style={{ color: '#B1ABAB', fontSize: '13px' }}>Already have an account? <Link to='/login'><span>Login Now</span></Link></p>
+        </div>
         <Copyright />
       </div>
-      
-    </div> 
-    
+
+    </div>
+
   );
 }
 
